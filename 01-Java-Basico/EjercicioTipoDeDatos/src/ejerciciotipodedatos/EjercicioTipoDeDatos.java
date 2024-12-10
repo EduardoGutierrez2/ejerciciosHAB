5ec03656564f1190da37835545340abf3a7b92d7
EduardoGutierrez2 <edugutierrez@gmail.com>
2024-12-09 18:48:31 -0600

    Agrego ejercicios de tipos de datos

---
+++ 01-Java-Basico/EjercicioTipoDeDatos/src/ejerciciotipodedatos/EjercicioTipoDeDatos.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciotipodedatos;
+
+/**
+ *
+ * @author edubi
+ */
+public class EjercicioTipoDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // a)
+        String nombre = "Eduardo";
+        int año = 2024;
+        double generacion = 1.1;
+        
+        System.out.println("Bienvenido " + nombre + " a Hack a Boss " + año + " generación " + generacion);
+        
+      
+       
+    }
+    
+}
---
+++ 01-Java-Basico/PruebaCambioDeDatos/src/pruebacambiodedatos/PruebaCambioDeDatos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebacambiodedatos;
+
+/**
+ *
+ * @author edubi
+ */
+public class PruebaCambioDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // int edad = "Hola" ;
+       // String nombre = 25;
+        char letra = 2;
+       //boolean bandera = 23;
+        double estatura = 202444;
+        //long dinero = "año 2024";
+        
+        System.out.println(estatura);
+    }
+    
+}
